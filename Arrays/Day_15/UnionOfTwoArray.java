47f6343b8201c8b231a50df600c2f6c252bdb523
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-23 14:25:41 +0545

    Find Union of two arrays with broute force approach.

--- Arrays/Day_15/UnionOfTwoArray.java
+++ Arrays/Day_15/UnionOfTwoArray.java
@@ -1,16 +1,21 @@
 import java.util.*;
-public class IntersectionOfTwoArray {
+public class UnionOfTwoArray {
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter first array size: ");
         int n1 = sc.nextInt();
+
         int[] arr1 = new int[n1];
         System.out.print("Enter first araay elements: ");
         for (int i = 0; i < arr1.length; i++) {
              arr1[i] = sc.nextInt();
         }
+
+
         System.out.print("Enter second array size: ");
         int n2 = sc.nextInt();
+
         int[] arr2 = new int[n2];
         System.out.print("Enter second araay elements: ");
         for (int i = 0; i < arr2.length; i++) {
@@ -18,11 +23,11 @@ public class IntersectionOfTwoArray {
         }
 
 
-        int n3 = (Math.min(arr1.length,arr2.length));
+        int n3 = arr1.length + arr2.length;
         int[] arr3 = new int[n3];
         int pos = 0;
-        System.out.print("Intersection of arr1 and arr2: ");
-        
+        System.out.print("Union of arr1 and arr2: ");
+        //copy arr1 in arr3 with visited logic.
         for (int i = 0; i < arr1.length; i++) {
             boolean visited = false;
             for(int j = 0; j < i; j++){
@@ -32,14 +37,28 @@ public class IntersectionOfTwoArray {
                 }
             }
             if(visited) continue;
-            for (int j = 0; j < arr2.length; j++) {
-                if(arr1[i] == arr2[j]){
-                    arr3[pos] = arr1[i];
-                    pos++;
+            arr3[pos] = arr1[i];
+            pos++;
+        }
+
+        //check if arr2[i] present in arr3[j]
+        //if yes then skip else store in arr3[pos] and pos++
+        for (int i = 0; i < arr2.length; i++){
+            boolean visited = false;
+            for (int j = 0; j < pos; j++) {
+                if(arr2[i] == arr3[j]){
+                    visited = true;
                     break;
                 }
             }
+                if (visited) continue;
+
+                arr3[pos] = arr2[i];
+                pos++;
         }
-        for(int j = 0; j < pos; j++) System.out.print(arr3[j] + " ");
+        //print arr3[i] till pos.
+        for (int i = 0; i < pos; i++) {
+             System.out.print(arr3[i] + " ");   
+        }
     }
 }
