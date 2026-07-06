0b5aea5f458aca7d986a23f48c949af705cff0a1
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-07-06 12:21:42 +0545

    Rearrange the array by the sign.

---
+++ Arrays/Day_26/RearrangeArrayBySign.java
@@ -0,0 +1,21 @@
+public class RearrangeArrayBySign{
+    public static void main(String[] args) {
+        int[] arr = {3 ,-2, 1, -5, 2, -4};
+
+        int[] ans = new int[arr.length];
+        int positiveIndex = 0;
+        int negativeIndex = 1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > 0){
+                ans[positiveIndex] = arr[i];
+                positiveIndex += 2;
+            } else {
+                ans[negativeIndex] = arr[i];
+                negativeIndex += 2;
+            }
+        }
+        for(int i = 0; i < ans.length; i++){
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
